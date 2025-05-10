5873c8739c6069ff1d51815a0e3f240d1a8c7f13
Jay Nguyen <jay@nguyens.co>
2025-05-10 12:31:53 +0100

    1/n layout

--- lib/proofwith_web/live/application_live/organizations.ex
+++ lib/proofwith_web/live/application_live/organizations.ex
@@ -2,11 +2,19 @@ defmodule ProofwithWeb.ApplicationLive.Organizations.Organization do
   @moduledoc false
   use ProofwithWeb, :live_view
 
+  def mount(socket) do
+    current_scope = socket.assigns.current_scope
+
+    socket = assign(socket, current_scope: current_scope)
+
+    {:ok, socket}
+  end
+
   def render(assigns) do
     ~H"""
-    <div>
+    <Layouts.app flash={@flash} current_scope={@current_scope}>
       <h1>Organizations</h1>
-    </div>
+    </Layouts.app>
     """
   end
 end
